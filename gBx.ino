cf4fba8118c33963a059b1512fd77148d6b9dbf2
Michael Trommer <mictro@gmail.com>
2019-05-26 16:08:19 +0200

    hygros suck, bme280 added

--- gBx.ino
+++ gBx.ino
@@ -1,20 +1,27 @@
+#include <Ethernet.h>
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#define BMP_SCK  (13)
+#define BMP_MISO (12)
+#define BMP_MOSI (11)
+#define BMP_CS   (10)
+
+
+Adafruit_BME280 bme; // I2C
+
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
 #include "ThingSpeak.h"
 
-#include <ArduinoJson.h>
-
-#include <SPI.h>
-#include <Ethernet.h>
-
-#include <Wire.h>
-
 // DS18B20 / OneWire
 #define ONE_WIRE_BUS 2
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
-
 
 const char * myCounterReadAPIKey = "";
 unsigned int counterFieldNumber = 1;
@@ -28,23 +35,12 @@ byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192, 168, 0, 146);
 IPAddress myDns(8, 8, 8, 8);
 
-// Initialize the Ethernet client library
-// with the IP address and port of the server
-// that you want to connect to (port 80 is default for HTTP):
 EthernetClient client;
-
 int number = 0;
-
 
 // Addresses of 2 DS18B20s
 uint8_t sensor1[8] = { 0x28, 0xAA, 0xCF, 0xB3, 0x1D, 0x13, 0x02, 0x86 };
 uint8_t sensor2[8] = { 0x28, 0xCD, 0xD1, 0xF0, 0x32, 0x14, 0x01, 0x31 };
-
-
-const int soilHygrometers[] = {
-  A0,
-  A1
-};
 
 
 typedef struct {
@@ -56,8 +52,15 @@ typedef struct {
 
 void setup(void) {
   Serial.begin(9600);
+  Serial.println("foobar");
 
+  unsigned bmeStatus;
   
+  bmeStatus = bme.begin();
+                  
+  Serial.println(bmeStatus);
+
+                  
   // start the Ethernet connection:
   Serial.println("Initialize Ethernet with DHCP:");
   if (Ethernet.begin(mac) == 0) {
@@ -82,6 +85,8 @@ void setup(void) {
   delay(1000);
 
 
+
+
   ThingSpeak.begin(client); 
   sensors.begin();
 }
@@ -89,10 +94,11 @@ void setup(void) {
 void loop(void) {
   sensors.requestTemperatures();
 
+
   Result result[RESULT_SIZE];
 
   result[0].ident = "tmp0";
-  result[0].value = 0;
+  result[0].value = bme.readTemperature();
 
   // DS18B20
   result[1].ident = "tmp1";
@@ -101,17 +107,12 @@ void loop(void) {
   result[2].ident = "tmp2";
   result[2].value = getTemperature(sensor2);
 
+  result[3].ident = "hum0";
+  result[3].value = bme.readHumidity();
+  
+  result[4].ident = "pressure0";
+  result[4].value = bme.readPressure();
 
-  int resOffset = 3;
-
-  String ident;
-   // hygros
-  for (int i=0; i<sizeof soilHygrometers/sizeof soilHygrometers[0]; i++) {
-    int s = soilHygrometers[i];
-    ident = "hygro" + String(i);
-    result[resOffset + i].ident = ident;
-    result[resOffset + i].value = getSoilHumidity(s);
-  };
   
   for (int i=0; i<sizeof result/sizeof result[0]; i++) {
     Result s = result[i];
@@ -139,18 +140,11 @@ void loop(void) {
   if(number > 99){
     number = 0;
   }
-  delay(30000);
+  delay(1000);
 }
 
 
 double getTemperature(DeviceAddress deviceAddress) {
   float tempC = sensors.getTempC(deviceAddress);
   return tempC;
-}
-
-int getSoilHumidity(int analogDevice) {
-  int value = analogRead(analogDevice);
-  value = constrain(value,400,1023); 
-  value = map(value,400,1023,100,0);
-  return value; 
 }
